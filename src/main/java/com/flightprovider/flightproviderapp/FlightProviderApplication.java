e350fa06ad35b858e2b209480381238c860c48d6
Ferit Unlu <feritunlu@hotmail.com>
2019-09-08 20:40:45 +0300

    -Initial commit

---
+++ src/main/java/com/flightprovider/flightproviderapp/FlightProviderApplication.java
@@ -0,0 +1,11 @@
+package com.flightprovider.flightproviderapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightProviderApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FlightProviderApplication.class, args);
+	}
+}
